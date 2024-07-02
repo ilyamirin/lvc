ded1db939361406a8b6bd1f2ef5b0b8707c60a50
mirin.ilya <Mirin.Ilya@wb.ru>
2024-07-02 16:42:00 +0300

    пробую транспорт

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,18 +1,25 @@
 import os
 
+import httpx
+from httpcore import AsyncConnectionPool, ConnectionPool
 from icecream import ic
-
 from linkvideo_client_wildberries_client import AuthenticatedClient
 from linkvideo_client_wildberries_client.api.cameras import get_cameras_all_points
-from linkvideo_client_wildberries_client.models import WBCamerasPoints
+from linkvideo_client_wildberries_client.models import WBCamerasPoints, WBCameraPoint
 
 
 def test_basic_operations():
     token: str = os.getenv("LINKVIDEO_TOKEN")
     ic(token)
-    client = AuthenticatedClient(base_url="https://api.b2o.goodline.info/ords/mobile/vc2", token=token)
+    transport = httpx.HTTPTransport(local_address="10.109.172.4")
+
+    client = AuthenticatedClient(base_url="https://api.b2o.goodline.info/ords/mobile/vc2",
+                                 token=token,
+                                 httpx_args={"transport": transport})
 
     with client:
         points_and_cameras: WBCamerasPoints = get_cameras_all_points.sync(client=client)
         assert isinstance(points_and_cameras, WBCamerasPoints)
         ic(points_and_cameras)
+
+        cameras: list[WBCameraPoint] = points_and_cameras.cameras
